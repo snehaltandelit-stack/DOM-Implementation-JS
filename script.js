e0790ec0b651560ef9fb5df9d95dbc0c08668f6f
snehaltandelit-stack <snehaltandelit@gmail.com>
2025-12-19 23:30:31 +0530

    Added 3 file
    index.html
    style.css
    script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+function funuser(){
+    const user =document.getElementById('txtuser');
+    const huser =document.getElementById('huser');
+    huser.innerText=huser.textContent + ", " + user.value;
+    user.value="";
+}
+
+
+// function changecolor(){
+// const redbox =document.getElementById('redbox');
+// redbox.classList.add("red");
+// const bluebox =document.getElementById('bluebox');
+// bluebox.classList.add("blue");
+// const greenbox =document.getElementById('greenbox');
+// greenbox.classList.add("green");
+// const yellowbox =document.getElementById('yellowbox');
+// yellowbox.classList.add("yellow");
+// }
+function redcolor(){
+const redbox =document.getElementById('redbox');
+redbox.classList.add("red");
+}
+function bluecolor(){
+const bluebox =document.getElementById('bluebox');
+bluebox.classList.add("blue");
+}
+function yellowcolor(){
+const yellowbox =document.getElementById('yellowbox');
+yellowbox.classList.add("yellow");
+}
+function greencolor(){
+const greenbox =document.getElementById('greenbox');
+greenbox.classList.add("green");
+}
