35b1812e0c566da78ae7b1fc023e8e45e69f90f3
snehaltandelit-stack <snehaltandelit@gmail.com>
2026-01-09 18:49:55 +0530

    Name function update
    if condition implimentation

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-function funuser(){
-    const user =document.getElementById('txtuser');
-    const huser =document.getElementById('huser');
-    const btngreet =document.getElementById('btngreet');
-    huser.innerText=huser.textContent + ", " + user.value;
-    // user.value="";
+function funuser() {
+  const user = document.getElementById("txtuser");
+  const huser = document.getElementById("huser");
+  const btngreet = document.getElementById("btngreet");
+  if (user.value != "") {
+    huser.innerText = huser.textContent + ", " + user.value;
     btngreet.disabled = true;
+  }
 }
 
-
-// function changecolor(){
-// const redbox =document.getElementById('redbox');
-// redbox.classList.add("red");
-// const bluebox =document.getElementById('bluebox');
-// bluebox.classList.add("blue");
-// const greenbox =document.getElementById('greenbox');
-// greenbox.classList.add("green");
-// const yellowbox =document.getElementById('yellowbox');
-// yellowbox.classList.add("yellow");
-// }
-function redcolor(){
-const redbox =document.getElementById('redbox');
-redbox.classList.add("red");
+function redcolor() {
+  const redbox = document.getElementById("redbox");
+  redbox.classList.add("red");
 }
-function bluecolor(){
-const bluebox =document.getElementById('bluebox');
-bluebox.classList.add("blue");
+function bluecolor() {
+  const bluebox = document.getElementById("bluebox");
+  bluebox.classList.add("blue");
 }
-function yellowcolor(){
-const yellowbox =document.getElementById('yellowbox');
-yellowbox.classList.add("yellow");
+function yellowcolor() {
+  const yellowbox = document.getElementById("yellowbox");
+  yellowbox.classList.add("yellow");
 }
-function greencolor(){
-const greenbox =document.getElementById('greenbox');
-greenbox.classList.add("green");
+function greencolor() {
+  const greenbox = document.getElementById("greenbox");
+  greenbox.classList.add("green");
 }
